a3838e670665f496bea9dcf7e6d90cc7dc1e9d83
FYH620 <78263637+FYH620@users.noreply.github.com>
2022-07-06 13:01:24 +0800

    update
    WHAT I DO:
    1.完成MultiBoxLoss
    TO DO:
    1.训练代码，调试观察Loss，确认代码正确性

--- utils/utils_multibox_loss.py
+++ utils/utils_multibox_loss.py
@@ -20,18 +20,63 @@ class MultiboxLoss(nn.Module):
         self.anchor_boxes_coords = AnchorBoxes().getAnchorBoxesForEachImage()
         self.loc_variance = [0.1, 0.2]
 
-    def forward(self, loc_data, conf_data, targets):
-        num_batch = loc_data.size(0)
+    def forward(self, predict_offset_coords, predict_confidences, targets):
+        num_batch = len(predict_offset_coords)
         anchor_boxes = self.anchor_boxes.copy()
-        num_anchors = anchor_boxes.size(0)
-        loc_t = torch.tensor(num_batch, num_anchors, 4)
-        conf_t = torch.LongTensor(num_batch, num_anchors, self.num_classes)
+        num_anchors = len(anchor_boxes)
+        offset_coords_labels = torch.tensor(num_batch, num_anchors, 4)
+        confidence_labels = torch.LongTensor(num_batch, num_anchors)
         for index in range(len(num_batch)):
-            loc_t[index], conf_t[index] = self._match(targets[index].data)
+            offset_coords_labels[index], confidence_labels[index] = self._match(
+                targets[index].data
+            )
 
         if self.use_cuda:
-            loc_t = loc_t.cuda()
-            conf_t = conf_t.cuda()
+            offset_coords_labels = offset_coords_labels.cuda()
+            confidence_labels = confidence_labels.cuda()
+
+        is_positive_anchors = confidence_labels > 0
+        positive_coords_labels = offset_coords_labels[
+            is_positive_anchors.unsqueeze(dim=is_positive_anchors.dim()).expand_as(
+                offset_coords_labels
+            )
+        ].view(-1, 4)
+        predict_positive_coords = predict_offset_coords[is_positive_anchors].view(-1, 4)
+        loss_location = F.smooth_l1_loss(
+            predict_positive_coords,
+            positive_coords_labels,
+            size_average=False,
+        )
+
+        num_positive = is_positive_anchors.sum(dim=-1, keepdim=True)
+        num_total_positive = num_positive.data.sum()
+        num_negative = torch.clamp(
+            self.neg_pos_ratio * num_positive,
+            max=num_anchors - 1,
+        )
+        loss_classification_auxiliary = self._negativeLogSoftmaxClassficationLoss(
+            predict_confidences.view(-1, self.num_classes),
+            confidence_labels.view(-1, 1),
+        ).view(num_batch, -1)
+        loss_classification_auxiliary[is_positive_anchors] = 0
+        _, loss_classification_auxiliary_rank = loss_classification_auxiliary.sort(
+            dim=1,
+            descending=True,
+        )
+        _, index_rank = loss_classification_auxiliary_rank.sort(dim=1)
+        neg_index = index_rank < (num_negative.expand_as(index_rank))
+        neg_mask = neg_index.unsqueeze(neg_index.dim()).expand_as(predict_confidences)
+        pos_mask = is_positive_anchors.unsqueeze(is_positive_anchors.dim()).expand_as(
+            predict_confidences
+        )
+        predict_c = predict_confidences[(pos_mask + neg_mask).gt(0)].view(
+            -1, self.num_classes
+        )
+        label_c = confidence_labels[(is_positive_anchors + neg_index).gt(0)]
+        loss_classification = F.cross_entropy(predict_c, label_c, size_average=False)
+        loss_location /= num_total_positive
+        loss_classification /= num_total_positive
+        return loss_location, loss_classification
 
     def _match(self, target):
         target_point_coords = target[:, :-1]
@@ -58,3 +103,14 @@ class MultiboxLoss(nn.Module):
         conf = target_class_labels[gt_index] + 1
         conf[gt_overlap < self.overlap_threshold] = 0
         return loc, conf
+
+    def _safeLogSumExpFunction(self, x):
+        x_max = torch.max(x, dim=1, keepdim=True)
+        return x_max + (x - x_max).exp().sum(dim=1, keepdim=True).log()
+
+    def _negativeLogSoftmaxClassficationLoss(self, x, gt_class_labels):
+        return self._safeLogSumExpFunction(x) - torch.gather(
+            x,
+            dim=1,
+            index=gt_class_labels,
+        )
